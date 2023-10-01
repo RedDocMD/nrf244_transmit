236023b77b6f0ae0f5dca7f76864dda3bbb69166
Deep Majumder <deep.majumder2019@gmail.com>
2023-10-01 13:46:00 +0530

    Made transmission look roughly like typing

--- nrf244_transmit.ino
+++ nrf244_transmit.ino
@@ -2,20 +2,63 @@
 #include <RF24_config.h>
 #include <nRF24L01.h>
 #include <LibPrintf.h>
+#include <Gaussian.h>
+#include "hid.h"
 
-const int CE = 7;
-const int CSN = 8;
-const int channel = 34;
+constexpr int CE = 7;
+constexpr int CSN = 8;
+constexpr int channel = 34;
 
 RF24 rf(CE, CSN);
 
 uint8_t msg[32];
-uint8_t addr[5] = {0xCD, 0x02, 0x03, 0x04, 0xCD};
+uint8_t addr[5] = {0x01, 0x02, 0x03, 0x04, 0xCD};
+
+struct Payload {
+  uint8_t dev_type;
+  uint8_t pkt_type;
+  uint8_t model;
+  uint8_t _unk;
+  uint16_t seq;
+  uint8_t flags;
+  uint8_t meta;
+  uint8_t _pad1;
+  uint8_t hid_code;
+  uint8_t _pad2[5];
+  uint8_t cksum;
+};
+
+Payload *pay = (Payload *)msg;
 
 void wait_forever() {
   while (true)
     asm volatile ("wfe");
 }
+
+void init_payload() {
+  pay->dev_type = 0x0A;
+  pay->pkt_type = 0x38;
+  pay->model = 0x06;
+  pay->seq = 0;
+  pay->flags = 0xFF;
+  pay->meta = 0x00;
+}
+
+constexpr int WPM = 100;
+constexpr int WPM_TO_CPM = 5;
+constexpr int CPM = WPM * WPM_TO_CPM;
+constexpr int MS_IN_MIN = 60 * 1000;
+
+constexpr int AVG_DELAY = MS_IN_MIN / CPM;
+constexpr int DELAY_VAR = 20;
+
+Gaussian delay_distr(AVG_DELAY, DELAY_VAR);
+
+constexpr int BURST_MIN = 30;
+constexpr int BURST_MAX = 50;
+constexpr int BURST_DELAY = 50;
+
+int burst_cnt, burst_lim;
 
 void setup() {
   Serial.begin(9600);
@@ -29,15 +72,20 @@ void setup() {
   rf.setAutoAck(false);
   rf.setPALevel(RF24_PA_MIN);
   rf.setDataRate(RF24_2MBPS);
-  rf.setPayloadSize(sizeof(msg));
+  rf.setPayloadSize(sizeof(Payload));
   rf.setChannel(channel);
+
+  init_payload();
 
   rf.setAddressWidth(sizeof(addr));
   rf.stopListening();
   rf.openWritingPipe(addr);
+
+  burst_cnt = 0;
+  burst_lim = random(BURST_MIN, BURST_MAX + 1);
 }
 
-void printMsg() {
+void print_msg() {
   printf("[");
   for (int i = 0; i < sizeof(msg); i++) {
     if (i != 0) printf(", ");
@@ -46,19 +94,36 @@ void printMsg() {
   printf("]");
 }
 
-void randMsg() {
-  for (int i = 0; i < sizeof(msg); i++) {
-    msg[i] = random(256);
+int get_delay() {
+  int delay = delay_distr.random();
+  if (burst_cnt == burst_lim) {
+    delay += BURST_DELAY;
+    burst_cnt = 0;
+    burst_lim = random(BURST_MIN, BURST_MAX + 1);
   }
+  return delay;
+}
+
+char random_ch() {
+  constexpr int TOT = 26 + 10 + 1;
+  int r = random(TOT);
+  if (r == 0) return ' ';
+  else if (r <= 10) return ('0' + r - 1);
+  else return ('a' + r - 11);
 }
 
 void loop() {
-  // randMsg();
-  rf.write(msg, sizeof(msg));
+  ++pay->seq;
+  char ch = random_ch();
+  pay->hid_code = hid_reverse(ch);
 
-  printf("Sent msg: ");
-  printMsg();
-  printf("\n");
+  printf("%c", ch);
+  rf.write(msg, sizeof(Payload));
 
-  delay(1000);
+  // printf("Sent msg: ");
+  // print_msg();
+  // printf("\n");
+
+  ++burst_cnt;
+  delay(get_delay());
 }
